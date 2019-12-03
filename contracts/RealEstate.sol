e89d908766d33306f34d2c1b02914f9d35d11a9b
Ariana Ocando <arianaocando@oxoft.io>
2019-12-03 14:30:15 -0400

    created sold properties count fuction

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -4,6 +4,7 @@ contract RealEstate {
     address payable public seller;
     uint private balance;
     uint[] public index;
+    uint[] public soldProperties;
 
     struct PublishedProperty {
         string hash;
@@ -16,6 +17,9 @@ contract RealEstate {
     PublishedProperty[] public publishedProperties;
 
     event newProperty(string hash, string name, string description, uint price);
+    event newBalance(uint balance);
+    event buyProperty(uint index);
+    event getSoldProperties(uint index);
 
     constructor() public {
         seller = msg.sender;
@@ -39,25 +43,30 @@ contract RealEstate {
         index.push(publishedProperties.length);
         emit newProperty(hash, name, description, price);
     }
-    function properitiesCount() public view returns (uint[] memory) {
+    function propertiesCount() public view returns (uint[] memory) {
         return index;
+    }
+    function soldPropertiesCount() public view returns (uint[] memory) {
+        return soldProperties;
     }
     function getProperty(uint _index) public view returns (string memory, string memory, string memory, uint, bool) {
         PublishedProperty memory property = publishedProperties[_index];
         return (property.hash, property.name, property.description, property.price, property.sold);
     }
-    function buyAProperity(uint _index) public payable onlyBuyer returns (string memory) {
+    function buyAProperty(uint _index) public payable onlyBuyer returns (string memory) {
         balance += msg.value;
         publishedProperties[_index].sold = true;
-
+        soldProperties.push(_index);
+        emit getSoldProperties(_index);
+        emit buyProperty(_index);
         return "Welcome! Home Sweet Home.";
     }
     function getBalance() public view onlySeller returns (uint) {
         return balance;
     }
-    function transferBalance() public payable onlySeller withdrawalBalance returns (string memory) {
+    function transferBalance() public payable onlySeller withdrawalBalance {
         seller.transfer(balance);
         balance = 0;
-        return "You have successfully withdrawn your balance";
+        emit newBalance(balance);
     }
 }
